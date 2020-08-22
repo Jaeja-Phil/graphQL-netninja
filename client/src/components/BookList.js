874718d2ed1d8c2a53788f0d855936ad4ab1cf40
Phillip Choi <jaeja-phil@gmail.com>
2020-08-22 21:05:51 +0900

    add functionality finished

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -6,13 +6,24 @@ import { getBooksQuery } from '../queries/queries';
 import BookDetails from './BookDetails';
 
 class BookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null,
+    };
+  }
+
   displayBooks() {
     const data = this.props.data;
     if (data.loading) {
       return <div>Loading Books...</div>;
     } else {
       return data.books.map((book) => {
-        return <li key={book.id}>{book.name}</li>;
+        return (
+          <li onClick={(e) => this.setState({ selected: book.id })} key={book.id}>
+            {book.name}
+          </li>
+        );
       });
     }
   }
@@ -21,7 +32,7 @@ class BookList extends Component {
     return (
       <div>
         <ul id='book-list'>{this.displayBooks()}</ul>
-        <BookDetails />
+        <BookDetails bookId={this.state.selected} />
       </div>
     );
   }
