5e476707ba9ca222fef8cf102b9ea1cbc3457f1b
Phillip Choi <jaeja-phil@gmail.com>
2020-08-22 20:41:34 +0900

    add bookdetail component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+// components
+import BookList from './components/BookList';
+import AddBook from './components/AddBook';
+// apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+});
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <ApolloProvider client={client}>
+      <div id='main'>
+        <h1>Phil's reading list</h1>
+        <BookList />
+        <AddBook />
+      </div>
+    </ApolloProvider>
+  );
 }
 
 export default App;
